da8d83d6c80d323b56e6d0b71e0bb1eecb1651f0
Starsky Eustache <star.eu88@gmail.com>
2016-08-11 23:30:48 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,9 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+greeting = "Hello World"
+
+age = 28
+
+name = "Starsky"
+
+puts greeting + " My name is #{name}, I am #{age} years old."
