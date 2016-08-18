1654dbeb53ce9cb3727a9dda678f2310b169399a
Zahara Chaudhry <chaudhryzahara@gmail.com>
2016-08-18 02:09:20 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,6 @@
- # Define a local variable called 'greeting',
+ # Define a glocal variable called 'greeting',
  # give it the value of the string "Hello World"
+
+greeting = "Hello World"
+
+puts greeting 
